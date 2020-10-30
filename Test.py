8d157fbe654352c26de077db1ddabfc223273380
Xiaoyu <391747621@qq.com>
2020-10-30 15:49:16 +0000

    Initial commit

---
+++ Test.py
@@ -0,0 +1,18 @@
+import pygame
+from pygame.locals import *
+import sys
+
+pygame.init()
+
+window = pygame.display.set_mode([500, 500])
+
+# 设置窗口标题
+pygame.display.set_caption("窗口标题")
+
+while True:
+    for event in pygame.event.get():  # 循环获取事件
+        if event.type == QUIT:  # 若检测到事件类型为退出，则退出系统
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()  # 更新屏幕内容
+
